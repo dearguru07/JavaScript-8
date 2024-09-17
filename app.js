7c0b8db8fd96ce70b2fe45389bcf492ccb27457b
dearguru07 <pangalaguruyadav101@gmail.com>
2024-09-17 10:09:57 +0530

    'js'

---
+++ app.js
@@ -0,0 +1,55 @@
+// -------reduces-----------
+
+let numbers=[1,2,3,4,5,6,7,8,9,25,4,8,2,55,58]
+
+// let result=numbers.reduce((max,curr)=>{
+// if(curr>max){
+//   max=curr
+// }
+// return max
+// },0)
+// console.log(result)
+
+// 1)---------highvalue----------
+
+// let result=numbers.reduce((x,y)=>{
+// if (x<y){
+//   x=y
+// }
+// return x
+// },0)
+// console.log(result)
+
+// 2)------small----------
+
+// let result=numbers.reduce((x,y)=>{
+// if(x>y){
+//   x=y
+// }
+// return x
+// })
+// console.log(result)
+
+// 3)---------total value----------
+
+// let result=numbers.reduce((total,curr)=>{
+//   total=total+curr
+//  return total
+// },0)
+// console.log(result)
+
+// ---------normal function and Arrow function------
+
+// let result=numbers.map(function(x){
+//   return x*x
+// })
+// console.log(result)
+
+// let result=numbers.map((x)=>{
+//   return x*x
+// })
+// console.log(result)
+
+
+
+// ------------document Object Model--------------
